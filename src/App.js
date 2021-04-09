8bc0c2b3fbd278e575d8396514bc4a18df2d488d
eyouale <teyouale@gmail.com>
2021-04-09 22:34:09 +0300

    Adding StateLess Form Control and Creating Resusable Flex Box Property( In mixins.js)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import {Container, Form} from "react-bootstrap";
-import './App.css';
+import { Container, Form } from "react-bootstrap";
+import styled from "styled-components";
+import mixins from "./styles/mixins";
+
+const StyledContainer = styled.div`
+  ${mixins.flexCenter};
+`;
 
 function App() {
   return (
+    <StyledContainer>
       <Container>
-          <Form>
-              <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Enter Your Github UserName</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-          </Form>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Enter Your Github UserName</Form.Label>
+            <Form.Control
+              size="lg"
+              type="text"
+              placeholder="Enter Your User Name"
+            />
+          </Form.Group>
+        </Form>
       </Container>
+    </StyledContainer>
   );
 }
 
